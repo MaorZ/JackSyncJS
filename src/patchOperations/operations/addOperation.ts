0c6f75badabe795b501e051b9881e5a1e76ae3dc
Maor Zigel <maorz1@gmail.com>
2016-11-14 00:16:16 +0200

    Added the AddOperation Apply implementation

--- src/patchOperations/operations/addOperation.ts
+++ src/patchOperations/operations/addOperation.ts
@@ -9,6 +9,27 @@ export class AddOpertaion extends PatchOperation {
   }
 
   apply(target: Object): Object {
+  	// Getting the parent container
+    let parent = this.opertaionPath.getContainerValue(target);
+    let key = this.opertaionPath.getLastToken();
+    if (parent instanceof Array) {
+      if (key === "-") {
+        parent.push(this.value);
+        return parent;
+      }
+      let index: number = Number(key);
+      if (index !== NaN) {
+        if (index === parent.length) {
+          parent.push(this.value);
+          return parent;
+        }
+        if (index < parent.length) {
+          parent.splice(index, 0, this.value);
+        }
+      }
+    } else {
+      parent[key] = this.value;
+    }
     return target;
   }
 
